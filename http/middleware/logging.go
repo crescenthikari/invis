740ecc947291d0ffac615c9dd507a99622b30019
Muhammad Fiqri Muthohar <viqronization@gmail.com>
2018-06-11 23:23:56 +0700

    update logging to measure running time

--- http/middleware/logging.go
+++ http/middleware/logging.go
@@ -3,13 +3,14 @@ package middleware
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Do stuff here
-		log.Println(r.RequestURI)
-		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		start := time.Now()
 		next.ServeHTTP(w, r)
+		end := time.Now()
+		log.Printf("| %v | [%s] | %q\n", end.Sub(start), r.Method, r.RequestURI)
 	})
 }
